f21b2f92e3c5fc1005e76db5564a99b7050cb87d
anuja <anujarosebaby@gmail.com>
2025-03-04 19:04:55 +0530

    db adapter

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calcualtor Functions
+"""
+
+def add(x,y):
+    return x + y
+
+def substract(x,y):
+    return x - y
+
+
---
+++ app/app/test.py
@@ -0,0 +1,23 @@
+"""
+sample tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """ TEst the calc module"""
+
+    def test_add_numbers(self):
+        """ test adding numbers together """
+        res = calc.add(5,6)
+
+        self.assertEqual(res,11)
+
+
+    def test_substract_numbers(self):
+        """ test substracting numbers  """
+        res = calc.substract(10,6)
+
+        self.assertEqual(res,4)
